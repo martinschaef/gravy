b8d226f3fe4bcd302dd79c1734db25b0c278c2a5
schaef <martinschaef@gmail.com>
2014-07-27 11:47:29 +0200

    - modified unit test.

--- gravy_test/src/gravy_test/InfeasibleCodeDetectionTest.java
+++ gravy_test/src/gravy_test/InfeasibleCodeDetectionTest.java
@@ -5,19 +5,63 @@ package gravy_test;
 
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.util.Collection;
+import java.util.LinkedList;
+
 import org.gravy.Options;
 import org.gravy.ProgramAnalysis;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  * @author schaef
  *
  */
+@RunWith(Parameterized.class)
 public class InfeasibleCodeDetectionTest {
 
+	@Parameterized.Parameters (name = "{index}: parse({1})")
+	public static Collection<Object[]> data() {
+		LinkedList<Object[]> filenames = new LinkedList<Object[]>();
+		  File dir = new File("./regression/infeasible_code");
+		  File[] directoryListing = dir.listFiles();
+		  if (directoryListing != null) {
+		    for (File child : directoryListing) {		    	
+		    	if (child.getName().endsWith(".bpl")) {
+		    		filenames.add(new Object[] {child.getAbsolutePath(), child.getName()});
+		    	} else {
+		    		//Ignore
+		    	}
+		    }
+		  } else {
+		    // Handle the case where dir is not really a directory.
+		    // Checking dir.isDirectory() above would not be sufficient
+		    // to avoid race conditions with another process that deletes
+		    // directories.
+		  }				  
+	   return filenames;
+   }
+	
+    private String input;
+    private String shortname;
+    public InfeasibleCodeDetectionTest(String input, String shortname) {
+        this.input = input;
+        this.shortname = shortname;
+    }
+
+		
 	@Test
 	public void test() {
-		String fname = "regression/infeasible_code/terpword.bpl";	
+		
+//		ProgramFactory pf = null;
+//		System.out.println("TEST: "+this.shortname);
+//		try {
+//			pf = new ProgramFactory(this.input);
+		System.out.println("TEST: "+this.shortname);
+		
+		String fname = this.input;	
 		
 		long expectedFeasibleBlocks = 56;
 		long expectedInfeasibleBlocks = 4;
